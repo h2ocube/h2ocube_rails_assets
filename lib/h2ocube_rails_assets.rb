dd4ea582603158291e5a623493bdd569b58de180
ben <ben@ben-ubuntu.(none)>
2013-02-16 17:15:07 +0800

    remove susy

--- lib/h2ocube_rails_assets.rb
+++ lib/h2ocube_rails_assets.rb
@@ -1,11 +1,8 @@
-['slim', 'sass-rails', 'coffee-rails', 'v8', 'uglifier', 'quiet_assets'].each{ |g| require g }
+['slim', 'sass-rails', 'compass-rails', 'coffee-rails', 'v8', 'uglifier', 'quiet_assets'].each{ |g| require g }
 
 module H2ocubeRailsAssets
   module Rails
 		class Rails::Engine < ::Rails::Engine
-		  initializer 'h2ocube_rails_assets.require' do
-		    require 'compass-rails'
-		  end
 		end
   end
 end
