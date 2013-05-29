5c36cddaf248fc5748e64a887d2339cb00fa5f1f
Ben <ben@zfben.com>
2013-05-29 20:51:40 +0800

    v0.0.18

--- test/jquery-ui_test.rb
+++ test/jquery-ui_test.rb
@@ -1,5 +1,8 @@
 describe 'jquery ui integration' do
   it 'jquery ui' do
+    visit '/assets/jquery.ui_test.js'
+    page.html.must_include 'jQuery UI'
+
     visit '/assets/jquery-ui_test.js'
     page.html.must_include 'jQuery UI'
   end
