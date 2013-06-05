b6c8b7f29c205f33cebea25542912feca6655923
Ben <ben@zfben.com>
2013-06-05 16:21:11 +0800

    fix test

--- test/turbolinks_test.rb
+++ test/turbolinks_test.rb
@@ -1,13 +1,11 @@
 describe 'turbolinks integration' do
   it 'turbolinks' do
     visit '/assets/turbolinks_test.js'
-    page.html.wont_include 'Error'
     page.html.must_include 'initializeTurbolinks'
   end
 
   it 'jquery.turbolinks' do
     visit '/assets/jquery-turbolinks_test.js'
-    page.html.wont_include 'Error'
-    page.html.must_include ''
+    page.html.must_include 'turbolinksReady'
   end
 end
