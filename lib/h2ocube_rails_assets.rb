70c6f8a18f8111ce609e7f1830ceadedaf87f979
Ben <ben@zfben.com>
2013-06-20 13:27:13 +0800

    remove rails

--- lib/h2ocube_rails_assets.rb
+++ lib/h2ocube_rails_assets.rb
@@ -1,7 +1,7 @@
-['slim', 'sass-rails', 'compass-rails', 'coffee-rails', 'v8', 'uglifier', 'turbolinks', 'quiet_assets'].each{ |g| require g }
+if defined?(Rails)
+  ['slim', 'sass-rails', 'compass-rails', 'coffee-rails', 'v8', 'uglifier', 'turbolinks', 'quiet_assets'].each{ |g| require g }
 
-module H2ocubeRailsAssets
-  if defined?(Rails)
+  module H2ocubeRailsAssets
     module Rails
       class Rails::Engine < ::Rails::Engine
         initializer 'h2ocube_rails_assets.require_dependency' do |app|
