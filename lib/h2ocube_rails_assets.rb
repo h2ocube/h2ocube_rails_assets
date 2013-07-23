0c16230173ea0b527237f1d0b8677c833a3980ee
Ben <ben@zfben.com>
2013-07-23 16:59:49 +0800

    try railte replace engine

--- lib/h2ocube_rails_assets.rb
+++ lib/h2ocube_rails_assets.rb
@@ -1,13 +1,12 @@
 if defined?(Rails)
   ['slim', 'sass-rails', 'compass-rails', 'coffee-rails', 'v8', 'uglifier', 'turbolinks', 'quiet_assets'].each{ |g| require g }
 
+  PATH = File.dirname(__FILE__)
   module H2ocubeRailsAssets
-    module Rails
-      class Rails::Engine < ::Rails::Engine
+      class Railtie < Rails::Railtie
         initializer 'h2ocube_rails_assets.require_dependency' do |app|
-
+          Dir[File.join(PATH, '../vendor/assets/*')].each{ |path| app.assets.append_path path }
         end
       end
-    end
   end
 end
