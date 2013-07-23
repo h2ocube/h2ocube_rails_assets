02e3fb79113558254b1d8c04ba5a2eab6cc01bda
Ben <ben@zfben.com>
2013-07-23 17:26:10 +0800

    try railte replace engine

--- lib/h2ocube_rails_assets.rb
+++ lib/h2ocube_rails_assets.rb
@@ -1,9 +1,9 @@
 if defined?(Rails)
   ['slim', 'sass-rails', 'compass-rails', 'coffee-rails', 'v8', 'uglifier', 'turbolinks', 'quiet_assets'].each{ |g| require g }
 
-  PATH = File.dirname(__FILE__)
   module H2ocubeRailsAssets
       class Railtie < Rails::Railtie
+        PATH = File.dirname(__FILE__)
         initializer 'h2ocube_rails_assets.require_dependency' do |app|
           app.config.assets.paths += Dir[File.join(PATH, '../vendor/assets/*')]
           app.config.assets.paths << Compass::Frameworks['compass'].stylesheets_directory
